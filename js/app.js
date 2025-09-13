2bd810b0c06832581a318c6cb959f50e1fe83d40
Yiğit Can Şahin <yigitcansahinben@gmail.com>
2025-09-13 13:58:12 +0300

    responsive

--- js/app.js
+++ js/app.js
@@ -19,8 +19,6 @@ $(document).ready(function() {
 
     //filtered-vehicles__slider
     var swiper = new Swiper(".filtered-vehicles__slider", {
-      slidesPerView: 5,
-      spaceBetween: 22,
       loop: true,
       centeredSlides: true,
       autoplay: {
@@ -34,6 +32,32 @@ $(document).ready(function() {
         nextEl: ".filtered-vehicles__next-button",
         prevEl: ".filtered-vehicles__prev-button",
       },
+      breakpoints: {
+        // mobil (0 - 575px)
+        0: {
+          slidesPerView: 2,
+          spaceBetween: 10,
+        },
+        // tablet (≥576px)
+        576: {
+          slidesPerView: 3,
+          spaceBetween: 15,
+        },
+        // küçük laptop (≥768px)
+        768: {
+          slidesPerView: 3,
+          spaceBetween: 20,
+        },
+        // normal laptop (≥992px)
+        992: {
+          slidesPerView: 4,
+          spaceBetween: 25,
+        },
+        1200: {
+          slidesPerView: 5,
+          spaceBetween: 22,
+        }
+      }
     });
 
 
